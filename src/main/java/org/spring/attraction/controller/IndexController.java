0445cb0fb722ed1e5485963e95fbfb68ded06d2f
seung <seungchan753@naver.com>
2025-03-11 14:28:53 +0900

    관광지 이미지 추가

--- src/main/java/org/spring/attraction/controller/IndexController.java
+++ src/main/java/org/spring/attraction/controller/IndexController.java
@@ -1,5 +1,7 @@
 package org.spring.attraction.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) principal;
+            model.addAttribute("userLoginId", userDetails.getUsername());
+        }
         return "index";
     }
 
